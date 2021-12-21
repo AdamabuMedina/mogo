4cda6df0a104f33865948d12138b76c2eb556f50
adamabumedina <adamabujamila@gmail.com>
2021-12-21 22:14:27 +0300

    Настройка gulp (wathc, css)

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -20,17 +20,17 @@ const task = requireDir("./task", { recurse: true })
 
 // Наблюдение
 const wathcer = () => {
-  watch($.path.html.watch, task.html).on("all", $.browsersync.reload);
-  watch($.path.scss.watch, task.scss).on("all", $.browsersync.reload);
-  watch($.path.css.watch, task.css).on("all", $.browsersync.reload);
-  watch($.path.js.watch, task.js).on("all", $.browsersync.reload);
-  watch($.path.img.watch, task.img).on("all", $.browsersync.reload);
-  watch($.path.font.watch, task.font).on("all", $.browsersync.reload);
+  $.gulp.watch($.path.html.watch, task.html).on("all", $.browsersync.reload);
+  // watch($.path.scss.watch, task.scss).on("all", $.browsersync.reload);
+  $.gulp.watch($.path.css.watch, task.css).on("all", $.browsersync.reload);
+  $.gulp.watch($.path.js.watch, task.js).on("all", $.browsersync.reload);
+  $.gulp.watch($.path.img.watch, task.img).on("all", $.browsersync.reload);
+  // watch($.path.font.watch, task.font).on("all", $.browsersync.reload);
 }
 
 const build = $.gulp.series(
   task.clear,
-  $.gulp.parallel(task.html, task.scss, task.js, task.img, task.font)
+  $.gulp.parallel(task.html, task.css, task.js, task.img,)//task.font, task.scss
 )
 
 const dev = $.gulp.series(
@@ -40,11 +40,11 @@ const dev = $.gulp.series(
 
 // Задачи
 exports.html = task.html;
-exports.scss = task.scss;
+// exports.scss = task.scss;
 exports.css = task.css;
 exports.js = task.js;
 exports.img = task.img;
-exports.font = task.font;
+// exports.font = task.font;
 exports.clear = task.clear;
 
 
